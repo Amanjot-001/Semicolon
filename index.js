f3eb611270644217f7ea859399267be6e0c0cfc1
Amanjot Singh <98868918+Amanjot-001@users.noreply.github.com>
2023-06-05 10:21:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const moment = require('moment');
+const session = require('express-session');
 let data = '';
-let topicIndex=0, subjectIndex=0, unitIndex=0, content;
+let userData = '';
+let sessionId = '';
+let topicIndex = 0, subjectIndex = 0, unitIndex = 0, content;
 
 app.use("/styles", express.static(__dirname + "/styles"));
 app.use("/scripts", express.static(__dirname + "/scripts"));
@@ -21,89 +26,256 @@ mongoose
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+app.use(session({
+  secret: '12121212',
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
+
+
 const Sem1 = new mongoose.Schema({
-    sem:
-      [
-        {
-          subjects:
-            [{
-              subjectName: String,
-              units:
-                [
-                  {
-                    unitName: String,
-                    topics:
-                      [
-                        {
-                          name: String,
-                          content: String,
-                        },
-                      ]
-                  },
-                ]
-            },
-            ]
-        },
-      ]
-  })
-  
-  const Sem1Notes = mongoose.model('Sem1Notes', Sem1);
-  async function fetchData() {
-    data = await Sem1Notes.find({});
+  sem:
+    [
+      {
+        subjects:
+          [{
+            subjectName: String,
+            units:
+              [
+                {
+                  unitName: String,
+                  topics:
+                    [
+                      {
+                        name: String,
+                        content: String,
+                      },
+                    ]
+                },
+              ]
+          },
+          ]
+      },
+    ]
+})
+
+const Sem1Notes = mongoose.model('Sem1Notes', Sem1);
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name cannot be blank']
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: [true, 'Username cannot be blank']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password cannot be blank']
+  },
+  score: [
+    {
+      // type: mongoose.Schema.Types.ObjectId,
+      // ref: 'Score'
+      wpm: {
+        type: String
+      },
+      accuracy: {
+        type: String
+      }
+    }
+  ],
+  bestScore: {
+    type: Number,
+    default: 0
+  },
+  testsTaken: {
+    type: Number,
+    default: 0
+  },
+  joined: {
+    type: String,
+    default: moment().format('MMM Do YY')
+  },
+  rank: {
+    type: Number,
+    default: 0
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+//   const ScoreSchema = new mongoose.Schema([
+//     {
+//     wpm: String,
+//     accuracy: String
+//   },
+// ]);
+
+//   const Score = mongoose.model('Score', ScoreSchema);
+
+
+async function fetchData() {
+  data = await Sem1Notes.find({});
+}
+
+async function fetchUserData(req, res) {
+  userData = await User.find({ _id: req.session.user_id });
 }
 
 app.get('/dog', async (req, res) => {
-    res.json(data);
-  })
+  res.json(data);
+})
 
 
 app.get('/read', async (req, res) => {
-    res.render('read', {data});
+  res.render('read', { data });
 })
 
 app.get('/test', (req, res) => {
-    res.render('typing', {content});
+  res.render('typing', { content });
 })
 
 app.post('/topicType', (req, res) => {
-    content = req.body.content;
-    res.sendStatus(200);
+  content = req.body.content;
+  res.sendStatus(200);
 })
 
-app.get('/topicTest', (req,res) => {
-    res.render('typing', {content});
+app.get('/topicTest', (req, res) => {
+  res.render('typing', { content });
 })
 
 app.get('/show', (req, res) => {
-    res.render('show', {topicIndex, subjectIndex, unitIndex, data});
+  res.render('show', { topicIndex, subjectIndex, unitIndex, data });
 })
 
 app.get('/sendNotes', async (req, res) => {
-    res.json({ topicIndex, subjectIndex, unitIndex, data });
+  res.json({ topicIndex, subjectIndex, unitIndex, data });
 })
 
 app.post('/showPost', (req, res) => {
-    topicIndex = req.body.topicIndex;
-    subjectIndex = req.body.subjectIndex;
-    unitIndex = req.body.unitIndex;
-    console.log(topicIndex, unitIndex, subjectIndex);
-    res.sendStatus(200);
+  topicIndex = req.body.topicIndex;
+  subjectIndex = req.body.subjectIndex;
+  unitIndex = req.body.unitIndex;
+  console.log(topicIndex, unitIndex, subjectIndex);
+  res.sendStatus(200);
 })
 
-app.get('/signIn', (req,res) => {
+app.get('/signIn', (req, res) => {
   res.render('auth');
 })
 
-app.get("*", async(req,res) => {
-    res.sendFile(path.join(__dirname, "./index.html"))
-    if(data == '')
+app.post('/register', async (req, res) => {
+  const { name, username, password } = req.body;
+  let flag = false;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    name,
+    username,
+    password: hash
+  });
+
+  try {
+    await user.save();
+    flag = false;
+  } catch (error) {
+    if (error.code === 11000) {
+      flag = true;
+    } else {
+      flag = true;
+    }
+  }
+  res.render('auth', { flag });
+});
+
+app.post('/signIn', async (req, res) => {
+  const { username, password } = req.body;
+  let flag2 = true;
+  try {
+    const user = await User.findOne({ username });
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (validPassword) {
+      req.session.user_id = user._id;
+      sessionId = req.session.user_id;
+      console.log(req.session.user_id)
+      console.log(user._id)
+      flag2 = true;
+    }
+    else {
+      flag2 = false;
+    }
+    res.render('index' , {sessionId});
+  } catch (error) {
+    flag2 = false;
+    res.render('auth', { flag2 });
+  }
+})
+
+app.post('/updateScore', async (req, res) => {
+  const user = await User.findOne({ _id: req.session.user_id });
+
+  const S = req.body.score;
+  const A = req.body.accuracy;
+
+  user.score.push({
+    wpm: S,
+    accuracy: A
+  });
+
+  let tests = user.testsTaken;
+  tests++;
+  user.testsTaken = tests;
+
+  let bestScore = user.bestScore;
+  if (S > bestScore)
+    user.bestScore = S;
+
+  const users = await User.find({}).sort({ bestScore: -1 });
+
+  let rank = 1;
+  for (const user of users) {
+    user.rank = rank;
+    await user.save();
+    rank++;
+  }
+
+  await user.save();
+  console.log(user);
+  res.sendStatus(200);
+})
+
+
+app.get('/userfind', async (req, res) => {
+  const user = await User.find({});
+  // await User.deleteMany({});
+  res.send(user);
+})
+
+app.get('/profile', async (req, res) => {
+  try {
+    userData = await User.findOne({ _id: req.session.user_id });
+    console.log(req.session.user_id)
+    // res.send(userData)
+    res.render('profile', {userData})
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+app.get("/", async (req, res) => {
+  res.render('index', {sessionId});
+  if (data == '')
     await fetchData();
 })
 
 app.listen('8080', () => {
-    console.log('listneing');
+  console.log('listneing');
 })
